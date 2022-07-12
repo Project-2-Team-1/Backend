e01bf4c397c4bff1ba46aea253ff0cc82c1cfac3
cmseibel <cam.m.seib@gmail.com>
2022-07-12 11:46:54 -0700

    Write GET and POST request routes for User controller

--- Project-2-Team-1/src/main/java/com/revature/web/UserController.java
+++ Project-2-Team-1/src/main/java/com/revature/web/UserController.java
@@ -1,8 +1,40 @@
 package com.revature.web;
 
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class UserController {
+import com.revature.models.User;
+import com.revature.service.UserService;
 
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/users")
+public class UserController {
+	
+	@Autowired
+	private UserService userv;
+	
+	@GetMapping
+	public ResponseEntity<Set<User>> getAll() {
+		return ResponseEntity.ok(userv.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable("id") int id) {
+		return ResponseEntity.ok(userv.getById(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> register(@RequestBody User u) {
+		return ResponseEntity.ok(userv.add(u));
+	}
 }
