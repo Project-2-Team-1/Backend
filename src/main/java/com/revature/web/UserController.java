1064e0aab37c31d1694aeb7b262d38cfaa060cea
Jen Osborne <jander38@yahoo.com>
2022-07-15 13:55:06 -0400

    adding update functions to user service and controller

--- src/main/java/com/revature/web/UserController.java
+++ src/main/java/com/revature/web/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,5 +59,10 @@ public class UserController {
 	@PostMapping
 	public ResponseEntity<User> register(@RequestBody User u) {
 		return ResponseEntity.ok(userv.add(u));
+	}
+	
+	@PutMapping
+	public ResponseEntity<User> updateUser(@RequestBody User u){
+		return ResponseEntity.ok(userv.updateUser(u));
 	}
 }
